2dc1881ab9b59011073a7c3458178b7cdcae11fc
Robert Rudolph <searchforsense@gmail.com>
2021-01-26 16:45:53 +0100

    added logging to interceptor

--- src/service/interceptors.js
+++ src/service/interceptors.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const API_USER = process.env.REACT_APP_APIUSER ||window.REACT_APP_APIUSER;
+const API_USER = process.env.REACT_APP_APIUSER || window.REACT_APP_APIUSER;
 const API_PW = process.env.REACT_APP_APIPW || window.REACT_APP_APIPW;
+
+console.log('process.env.REACT_APP_APIUSER', process.env.REACT_APP_APIUSER)
+console.log('window.REACT_APP_APIUSER', window.REACT_APP_APIUSER)
+console.log('API_USER', API_USER);
 
 export const initInterceptors = () => {
     axios.interceptors.request.use(req => {
