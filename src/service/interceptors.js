dd84f6af95da5b7229e3d7e20f48fd1b48388c09
Robert Rudolph <searchforsense@gmail.com>
2021-01-27 11:42:25 +0100

    deleted logs

--- src/service/interceptors.js
+++ src/service/interceptors.js
@@ -3,10 +3,6 @@ import axios from "axios";
 const API_USER = process.env.REACT_APP_APIUSER || window.REACT_APP_APIUSER;
 const API_PW = process.env.REACT_APP_APIPW || window.REACT_APP_APIPW;
 
-console.log('process.env.REACT_APP_APIUSER', process.env.REACT_APP_APIUSER)
-console.log('window.REACT_APP_APIUSER', window.REACT_APP_APIUSER)
-console.log('API_USER', API_USER);
-
 export const initInterceptors = () => {
     axios.interceptors.request.use(req => {
         req.auth = {username: API_USER, password: API_PW};
