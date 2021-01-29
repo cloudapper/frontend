0ca71d0238d1a230d5674906b0c00dd2a7618e6a
Hendrik Ohrdes <hohrdes@gmail.com>
2021-01-29 15:14:15 +0100

    first keycloak try

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import { ReactKeycloakProvider } from '@react-keycloak/web'
+import keycloak from './keycloak'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+  <ReactKeycloakProvider authClient={keycloak}>
+      <App />
+  </ReactKeycloakProvider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
