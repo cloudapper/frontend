36b36c13c8d55c3533449674608436cf2e3b0ae1
Hendrik Ohrdes <hohrdes@gmail.com>
2021-02-02 21:54:51 +0100

    changed initial state of isLoggedIn to listenn to localStorage for refresh issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Login from './components/Login';
 
 const App = () => {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("fin") !== null)
 
   const loginCB = success => {
     setIsLoggedIn(success);
