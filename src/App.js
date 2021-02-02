b42040fd3f29fc7500f89291f2786968eeced06d
Hendrik Ohrdes <hohrdes@gmail.com>
2021-02-02 20:44:42 +0100

    added fin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, {useState, useEffect} from 'react';
 import CarDataView from './views/CarDataView';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
 import Login from './components/Login';
+import { getFin } from './service/AccountService';
 
 const App = () => {
 
@@ -23,7 +25,11 @@ const App = () => {
   return (
     <>
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand>Auto Daten List Informationen System (ADLIS)</Navbar.Brand>
+      <Navbar.Brand>Auto Daten List Informationen System (ADLIS) 
+        {isLoggedIn &&
+        <Badge pill variant="secondary" style={{fontSize: 10}}>FIN: {getFin()}
+        </Badge>}
+        </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
